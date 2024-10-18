37cc9a3c2aa6ece2ff0c0b9c2885ecce70ebc65a
lilonlyfans <105881824+vihtu@users.noreply.github.com>
2024-10-17 21:28:38 -0400

    Add files via upload

---
+++ bar nego server/static/js/products.js
@@ -0,0 +1,152 @@
+// Types:
+// 1 = Snack limpeza
+// 2 = Combos biscoito
+// 3 = Portions congelados
+// 4 = Drinks bebida
+
+
+
+
+
+
+const products = [
+    {
+        id: 1,
+        type: 1,
+        name: 'Detergente Líquido Lava',
+        description: 'Detergente Líquido Lava',
+        price: 16.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202307060125_6a9zwilxd3u.jpg?imwidth=256'
+    },
+    {
+        id: 2,
+        type: 1,
+        name: 'Detergente Líquido de Limão',
+        description: 'Detergente Líquido de Limão.',
+        price: 16.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202311021141_wcj7e7cp1tk.jpg?imwidth=256'
+    },
+    {
+        id: 3,
+        type: 1,
+        name: 'Sabonete em Barra Leite Nivea',
+        description: 'Sabonete em Barra Leite Nivea',
+        price: 18.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202401161144_pxy1yw99xdr.jpeg?imwidth=256'
+    },
+   
+
+// 2 
+    
+    {
+        id: 101,
+        type: 2,
+        name: 'Cookies Maxi Chocolate',
+        description: 'Cookies Maxi Chocolate.',
+        price: 30.99,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202403151402_lxi9hcpkff.png?imwidth=256'
+    },
+    {
+        id: 102,
+        type: 2,
+        name: 'Cookie Bauducco 100g Original',
+        description: 'Cookie Bauducco 100g Original.',
+        price: 30.99,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202403151402_lxi9hcpkff.png?imwidth=256'
+    },
+   
+    // 4
+   
+
+    
+    {
+        id: 103,
+        type: 4,
+        name: 'bhoema',
+        description: 'bhoema lata.',
+        price: 5.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202310101325_5bs9ls6wut4.jpg?imwidth=256  '
+    },
+    {
+        id: 104,
+        type: 4,
+        name: 'brama',
+        description: 'brama lata.',
+        price: 5.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202311031735_ikq2koijty.jpg?imwidth=256'
+    },
+    {
+        id: 105,
+        type: 4,
+        name: 'cristal',
+        description: 'cristal lata.',
+        price: 5.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202210182204_2ty2ui084qp.jpg?imwidth=256'
+    },
+   
+
+    {
+        id: 106,
+        type: 4,
+        name: 'Heineken',
+        description: 'Heineken lata.',
+        price: 10.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202307102256_lh2pyvfn79o.jpg?imwidth=256'
+    },
+   
+
+
+    {
+        id: 107,
+        type: 4,
+        name: 'brahma ',
+        description: 'brahma litrao.',
+        price: 10.00,
+        lastPrice: 0,
+        img: 'https://th.bing.com/th/id/OIP.XMn6olQA9MANRN8QwOW3SQHaHa?rs=1&pid=ImgDetMain  '
+    },
+    {
+        id: 108,
+        type: 4,
+        name: 'bud',
+        description: 'brama lata.',
+        price: 7.00,
+        lastPrice: 0,
+        img: 'https://th.bing.com/th/id/OIP.1oK0_zNyXzw84-xkasg_KAAAAA?rs=1&pid=ImgDetMain'
+    },
+    {
+        id: 109,
+        type: 4,
+        name: 'cristal',
+        description: 'cristal lata.',
+        price: 18.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202210182204_2ty2ui084qp.jpg?imwidth=256'
+    },
+   
+
+    {
+        id: 110,
+        type: 4,
+        name: 'Heineken',
+        description: 'Heineken lata.',
+        price: 18.00,
+        lastPrice: 0,
+        img: 'https://static.ifood-static.com.br/image/upload/t_low/pratos/820af392-002c-47b1-bfae-d7ef31743c7f/202307102256_lh2pyvfn79o.jpg?imwidth=256'
+    },
+   
+
+
+
+
+
+]
